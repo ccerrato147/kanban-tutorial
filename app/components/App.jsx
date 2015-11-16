de4d50db1dc18af5685e212556cf1c5790ba4223
Carlos Rene <ccerrato147@gmail.com>
2015-11-15 20:09:01 -0600

    Before adding lanes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,60 +1,46 @@
+import 'array.prototype.findindex';
 import uuid from 'node-uuid';
 import React from 'react';
 import Notes from './Notes.jsx';
-
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn Webpack'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Do laundry'
-  }
-];
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component{
   constructor(props){
     super(props);
-    this.state = {
-      notes: [
-        {
-          id: uuid.v4(),
-          task: 'Learn Webpack'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Learn React'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Do laundry'
-        }
-      ]
-    };
     this.addNote = this.addNote.bind(this);
+    this.editNote = this.editNote.bind(this);
+    this.deleteNote = this.deleteNote.bind(this);
+    this.storeChanged = this.storeChanged.bind(this);
+    this.state = NoteStore.getState();
+  }
+  componentDidMount() {
+    NoteStore.listen(this.storeChanged);
+  }
+  componentWillUnmount() {
+    NoteStore.unlisten(this.storeChanged);
+  }
+  storeChanged(state) {
+    // Without proper `bind`, `this` wouldn't
+    // point at the right context (defaults to `window`
+    // in browser environment)
+    this.setState(state);
   }
 	render() {
     return (
       <div>
         <button className="add-note" onClick={this.addNote}>+</button>
-        <Notes items={this.state.notes} onEdit={this.editNote} />
+        <Notes items={this.state.notes} onEdit={this.editNote} onDelete={this.deleteNote} />
       </div>
     );
   }
   addNote() {
-    this.setState({
-      notes:this.state.notes.concat([{
-        id: uuid.v4(),
-        task: 'New Task'
-      }])
-    });
+    NoteActions.create({task: 'New task'});
   }
-  editNote(noteId, task) {
-    console.log('note edited', noteId, task);
+  editNote(id, task) {
+    NoteActions.update({id, task});
+  }
+  deleteNote(id) {
+    NoteActions.delete(id);
   }
 }
